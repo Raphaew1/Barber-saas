9e9dc4b72862dcbd34da9b7e17011c70bf82426f
Raphael <raphacom.web@gmail.com>
2026-04-06 15:41:51 +0100

    Revert "Polish initial portal landing"
    This reverts commit 52b33e1964b57e9b10d603e2a10ff9c31c6e8aab.

--- app.js
+++ app.js
@@ -6330,7 +6330,6 @@ function updateLoginPortalUi() {
 function updatePortalLandingUi() {
   const shouldUseLandingLayout = isMainPortalLanding() && currentVisibleScreenId === 'login'
   document.body.classList.toggle('portal-landing', shouldUseLandingLayout)
-  document.documentElement.classList.toggle('initial-portal-landing', shouldUseLandingLayout)
 }
 
 // Exibe apenas os botoes de menu permitidos para o portal atual.
